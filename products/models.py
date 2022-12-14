c7f32739d33b648bf858bf9446e52f97b6c27b8f
Yakub Ali <mamuzaan@yahoo.com>
2022-12-14 23:02:14 +0000

    Customized admin

--- products/models.py
+++ products/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=255)
     friendly_name = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
+
     def get_friendly_name(self):
         return self.friendly_name
 
