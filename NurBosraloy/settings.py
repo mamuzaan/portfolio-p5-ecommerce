477008a0e1fb9b64176a3cffcbebdc1eb26a91c4
Yakub Ali <mamuzaan@yahoo.com>
2022-12-20 22:00:47 +0000

    Added checkout, views and templates

--- NurBosraloy/settings.py
+++ NurBosraloy/settings.py
@@ -53,6 +53,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    #  Others
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +69,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'NurBosraloy.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -81,8 +86,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
