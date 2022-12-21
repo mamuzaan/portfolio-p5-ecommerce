058f315d40d2fdaf67e0b75ed13cbfb7a4631fc5
Yakub Ali <mamuzaan@yahoo.com>
2022-12-21 21:21:43 +0000

    Added order summery to checkout success page

--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,9 @@
 import uuid
+
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+
 from products.models import Product
 
 
@@ -36,7 +38,7 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(
-            Sum('lineitem_total'))['lineitem_total__sum']
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -60,8 +62,10 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(
         Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True)  # XS, S, M, L, XL
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(
+        max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
